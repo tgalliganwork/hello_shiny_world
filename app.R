8b1593939fd40a78c42baa0b75070bf6d57d83e4
Thomas Galligan <thomas.galligan@gmail.com>
2021-01-08 14:55:08 +0000

    Tut 2.5 edit for ggplot summary and plot. Plot needs work.

--- app.R
+++ app.R
@@ -8,31 +8,25 @@
 #
 
 library(shiny)
+library(ggplot2)
+
+datasets <- data(package = "ggplot2")$results[c(2, 4, 10), "Item"]
 
 ui <- fluidPage(
-    sliderInput("x", "If x is", min = 1, max = 50, value = 30),
-    sliderInput("y", "and y is", min = 1, max = 50, value = 5),
-    "then, (x * y) is", textOutput("product"),
-    "and, (x * y) + 5 is", textOutput("product_plus5"),
-    "and (x * y) + 10 is", textOutput("product_plus10")
+  selectInput("dataset", "Dataset", choices = datasets),
+  verbatimTextOutput("summary"),
+  plotOutput("plot")
 )
 
 server <- function(input, output, session) {
-    dataset <- reactive({
-        input$x * input$y
-    })
-    
-    output$product <- renderText({ 
-        product <- dataset()
-    })
-   output$product_plus5 <- renderText({
-      product <- dataset()
-      product + 5
-   })
-   output$product_plus10 <- renderText({
-       product <- dataset()
-       product + 10
-   })
+  dataset <- reactive({
+    get(input$dataset, "package:ggplot2")
+  })
+  output$summary <- renderPrint({
+    summary(dataset())
+  })
+  output$plot <- renderPlot({
+    plot(dataset())
+  }, res = 20)
 }
-
 shinyApp(ui, server)
