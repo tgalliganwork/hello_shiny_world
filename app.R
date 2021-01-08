23b2c4ddea390dc6baf509b3d03718d1e91999fd
Thomas Galligan <thomas.galligan@gmail.com>
2021-01-08 13:34:35 +0000

    update app with second slider for y

--- app.R
+++ app.R
@@ -11,13 +11,15 @@ library(shiny)
 
 ui <- fluidPage(
     sliderInput("x", label = "If x is", min = 1, max = 50, value = 30),
-    "then x times 5 is",
+    "and y is",
+    sliderInput("y", label = "", min = 1, max = 50, value = 30),
+    "then x times y is",
     textOutput("product")
 )
 
 server <- function(input, output, session) {
     output$product <- renderText({ 
-        input$x * 5
+        input$x * input$y
     })
 }
 
